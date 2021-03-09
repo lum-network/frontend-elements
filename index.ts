86b3077e0bf72134de1b7840160a71b62778cb13
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-03-09 12:15:07 +0100

    Remove badge from frontend-elements

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@
 export { default as Loading } from './components/Loading/Loading';
 export { default as Button } from './components/Button/Button';
 export { default as CodeQr } from './components/CodeQr/CodeQr';
-export { default as Badge } from './components/Badge/Badge';
 export { default as TextInput } from './components/Inputs/TextInput';
 export { default as Card } from './components/Card/Card';
 export { default as Table } from './components/Table/Table';
