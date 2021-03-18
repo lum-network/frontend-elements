d9d5f347601d18786b42be12f80f767126bcb376
Segfault <tyldar@users.noreply.github.com>
2021-03-18 10:59:52 +0100

    Updated components to react hooks

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,22 +1,20 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import './Button.scss';
 
-interface IProps {
+interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     outline?: boolean;
     onPress: () => void;
     className?: string;
 }
 
-class Button extends PureComponent<IProps> {
-    render(): JSX.Element {
-        const { children, onPress, outline, className } = this.props;
+const Button = (props: IProps): JSX.Element => {
+    const { children, onPress, outline, className } = props;
 
-        return (
-            <div onClick={onPress} className={`app-btn ${outline ? 'app-btn-outline' : 'app-btn-plain'} ${className}`}>
-                {children}
-            </div>
-        );
-    }
-}
+    return (
+        <div onClick={onPress} className={`app-btn ${outline ? 'app-btn-outline' : 'app-btn-plain'} ${className}`}>
+            {children}
+        </div>
+    );
+};
 
 export default Button;
--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,15 +1,13 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
-class Loading extends PureComponent {
-    render(): JSX.Element {
-        return (
-            <div className="d-flex justify-content-center align-items-center">
-                <div className="spinner-grow" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
+const Loading = (): JSX.Element => {
+    return (
+        <div className="d-flex justify-content-center align-items-center">
+            <div className="spinner-grow" role="status">
+                <span className="visually-hidden">Loading...</span>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Loading;
