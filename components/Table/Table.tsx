fdca288cf2fb10ec6d9a475ce902df16dbd0ed5d
Karim Dalaize <karim.dalaize@epitech.eu>
2021-09-27 12:25:58 +0200

    [Updated] Customizable Table

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -3,15 +3,16 @@ import './Table.scss';
 
 interface IProps {
     children?: React.ReactNode;
+    className?: string;
     head: string[];
 }
 
 const Table = (props: IProps): JSX.Element => {
-    const { head, children } = props;
+    const { head, children, className } = props;
     const limitLeft = head.length / 2;
 
     return (
-        <div className="table-responsive">
+        <div className={`table-responsive ${className}`}>
             <table className="table app-table-striped table-borderless">
                 <thead>
                     <tr>
