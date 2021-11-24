16cb29b5262d7994497fdac1c14c48a37ae9afd2
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-11-24 15:09:27 +0100

    Bring changes to production (#8)

--- index.ts
+++ index.ts
@@ -5,3 +5,4 @@ export { default as CodeQr } from './components/CodeQr/CodeQr';
 export { default as TextInput } from './components/Inputs/TextInput';
 export { default as Card } from './components/Card/Card';
 export { default as Table } from './components/Table/Table';
+export { default as ValidatorLogo } from './components/ValidatorLogo/ValidatorLogo';
