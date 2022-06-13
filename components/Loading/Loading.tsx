14d1c73f8ce4585eaa477b32ef9e0d09da8f5ae1
Karim Dalaize <karim.dalaize@epitech.eu>
2022-06-13 15:44:57 +0200

    [Fixed] Dark mode loading

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Loading.scss';
+
 const Loading = (): JSX.Element => {
     return (
         <div className="d-flex justify-content-center align-items-center">
