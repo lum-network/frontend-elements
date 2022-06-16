99e2895a05da72cfd8215f6d207df6a9ba5c5e88
Thibault Jaillard <thibault.jaillard@gmail.com>
2022-06-16 17:59:50 +0200

    Bring changes to production (#12)
    * Updated components to react hooks
    
    * Add loading in Button
    
    * Fix button
    
    * update main.scss (#4)
    
    * [Updated] Customizable Table (#5)
    
    * Add table on proposals list
    
    * Update button dark theme color
    
    * Prettierify
    
    * Update vote grey color
    
    * Update color
    
    * Add Validators logo
    
    * Update validator logo filter in dark mode
    
    * Add new colors to main.scss
    
    * Add airdrop tooltip explications
    
    * [Updated] Button outline styles
    
    * [Updated] Button with proper disabled styles
    
    * Change node-sass to sass
    
    * [Fixed] Dark mode loading
    
    * Revert previous commit
    
    * [Fixed] Loading color for buttons
    
    * pagination wip
    
    * Pagination done
    
    * Blocks and delegations in validators done
    
    * Add first and last page
    
    * Improve design pagination
    
    * Update margin for pagination
    
    Co-authored-by: Segfault <tyldar@users.noreply.github.com>
    Co-authored-by: Karim Dalaize <karim.dalaize@epitech.eu>

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -23,20 +23,27 @@ const Table = (props: IProps): JSX.Element => {
             return null;
         }
 
-        const { page, hasPreviousPage, hasNextPage } = pagination;
+        const { page, hasPreviousPage, hasNextPage, pagesTotal } = pagination;
 
-        if (page === undefined) {
+        if (page === undefined || pagesTotal === undefined) {
             return null;
         }
 
         return (
-            <div className="pe-4 pb-4 pe-xl-5 pb-xl-5 d-flex justify-content-end">
+            <div className="pe-3 d-flex justify-content-end">
                 <ul className="pagination">
                     <li className={`page-item ${hasPreviousPage ? '' : 'disabled'}`}>
                         <a onClick={() => onPageChange(page - 1)} className="page-link pointer">
                             Previous
                         </a>
                     </li>
+                    {hasPreviousPage && page > 1 && (
+                        <li className="page-item">
+                            <a onClick={() => onPageChange(0)} className="page-link pointer">
+                                0 ...
+                            </a>
+                        </li>
+                    )}
                     {hasPreviousPage && (
                         <li className="page-item">
                             <a onClick={() => onPageChange(page - 1)} className="page-link pointer">
@@ -54,6 +61,13 @@ const Table = (props: IProps): JSX.Element => {
                             </a>
                         </li>
                     )}
+                    {hasNextPage && page < pagesTotal - 2 && (
+                        <li className="page-item">
+                            <a onClick={() => onPageChange(pagesTotal - 1)} className="page-link pointer">
+                                ... {pagesTotal - 1}
+                            </a>
+                        </li>
+                    )}
                     <li className={`page-item ${hasNextPage ? '' : 'disabled'}`}>
                         <a onClick={() => onPageChange(page + 1)} className="page-link pointer">
                             Next
@@ -65,21 +79,23 @@ const Table = (props: IProps): JSX.Element => {
     };
 
     return (
-        <div className={`table-responsive ${className}`}>
-            <table className="table app-table-striped table-borderless">
-                <thead>
-                    <tr>
-                        {head.map((value: string, index) => (
-                            <th className={limitLeft <= index ? 'text-end' : 'm-4'} key={index}>
-                                {value}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>{children}</tbody>
-            </table>
+        <>
+            <div className={`table-responsive ${className}`}>
+                <table className="table app-table-striped table-borderless">
+                    <thead>
+                        <tr>
+                            {head.map((value: string, index) => (
+                                <th className={limitLeft <= index ? 'text-end' : 'm-4'} key={index}>
+                                    {value}
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>{children}</tbody>
+                </table>
+            </div>
             {renderPagination()}
-        </div>
+        </>
     );
 };
 
