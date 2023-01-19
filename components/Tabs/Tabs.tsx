54a21e23f8eae1bbccb3673b0781a597be2f3c91
Thibault Jaillard <thibault.jaillard@gmail.com>
2023-01-19 20:41:18 +0100

    Add Tabs component (#14)
    * Updated components to react hooks
    
    * Add loading in Button
    
    * Fix button
    
    * update main.scss (#4)
    
    * [Updated] Customizable Table (#5)
    
    * Add table on proposals list
    
    * Update button dark theme color
    
    * Prettierify
    
    * Update vote grey color
    
    * Update color
    
    * Add Validators logo
    
    * Update validator logo filter in dark mode
    
    * Add new colors to main.scss
    
    * Add airdrop tooltip explications
    
    * [Updated] Button outline styles
    
    * [Updated] Button with proper disabled styles
    
    * Change node-sass to sass
    
    * [Fixed] Dark mode loading
    
    * Revert previous commit
    
    * [Fixed] Loading color for buttons
    
    * pagination wip
    
    * Pagination done
    
    * Blocks and delegations in validators done
    
    * Add first and last page
    
    * Improve design pagination
    
    * Update margin for pagination
    
    * Fix pagination color in dark mode
    
    * Add Tabs component
    
    * Prettierify
    
    Co-authored-by: Segfault <tyldar@users.noreply.github.com>
    Co-authored-by: Karim Dalaize <karim.dalaize@epitech.eu>

---
+++ components/Tabs/Tabs.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './Tabs.scss';
+
+type Tab = { name: string; id: number };
+
+interface IProps {
+    tabs: Tab[];
+    tabsContent: {
+        [id: number]: React.ReactNode;
+    };
+    className?: string;
+}
+
+const Tabs = ({ tabs, tabsContent, className }: IProps): JSX.Element => {
+    const [activeTab, setActiveTab] = React.useState(tabs[0].id);
+    const renderTabsTitle = (tab: Tab, index: number) => {
+        return (
+            <li onClick={() => setActiveTab(tab.id)} key={index} className={`tab-title ${tab.id === activeTab ? 'active' : ''}`}>
+                {tab.name}
+            </li>
+        );
+    };
+
+    return (
+        <div className={`tabs ${className}`}>
+            <div className="d-flex justify-content-center">
+                <ul className="tabs-header">{tabs.map((tab, index) => renderTabsTitle(tab, index))}</ul>
+            </div>
+            <div className="tabs-content h-100">{tabsContent[activeTab]}</div>
+        </div>
+    );
+};
+
+export default Tabs;
